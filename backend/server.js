e734451a91328d8a094f8ab813a09d446826ec43
Mekala. Mary Satvika <satvika2812@gmail.com>
2025-10-30 22:08:30 +0530

    Update CORS configuration and server listening address

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,15 @@ const app = express();
 
 // Security middleware
 app.use(helmet());
-app.use(cors());
+app.use(cors(
+  {
+  origin: [
+    https://spendr-frontend.onrender.com', 
+    'http://localhost:5000' 
+  ],
+  credentials: true
+}
+));
 app.use(express.json());
 
 // Rate limiting
@@ -38,6 +46,7 @@ app.get('/', (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
 });
