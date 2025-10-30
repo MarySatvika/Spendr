9546eb71d861eb1b7aa4933761fb7e225f9835b9
Mekala. Mary Satvika <satvika2812@gmail.com>
2025-10-30 22:31:07 +0530

    Remove port configuration from Vite server

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
     proxy: {
       '/api': {
         target: 'https://spendr-96g4.onrender.com',
@@ -17,3 +16,5 @@ export default defineConfig({
     sourcemap: true
   }
 })
+
+
